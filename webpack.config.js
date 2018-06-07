1b70b7dec2dee23c0069ad4d13eb9fe62b5d427b
Rick <r.aikema@ultraware.nl>
2018-06-07 15:11:15 +0200

    Buld with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path')
 var config = {
   mode: 'development',
   output: {
-    path: path.resolve(__dirname, '/dist/'),
+    path: path.resolve(__dirname, './dist/'),
   },
   module: {
     rules: [
@@ -13,23 +13,29 @@ var config = {
         loader: 'babel-loader',
         include: [path.resolve(__dirname, './src')],
       },
+      {
+        test: /\.vue$/,
+        include: [path.resolve(__dirname, './src')],
+        loader: 'vue-loader',
+      },
     ],
   },
   externals: {
     quasar: 'quasar',
+    '@vue/test-utils': '@vue/test-utils',
   },
   resolve: {
-    extensions: ['.js'],
+    extensions: ['.js', '.vue', '.json'],
     alias: {
       quasar: path.resolve(__dirname, '../node_modules/quasar-framework/'),
       src: path.resolve(__dirname, './src'),
     },
   },
 }
-
+console.log(__dirname)
 module.exports = [
   merge(config, {
-    entry: path.resolve(__dirname, '/src/index.js'),
+    entry: path.resolve(__dirname, './src/index.js'),
     output: {
       filename: 'index.js',
       libraryTarget: 'umd',
